14baefb400d66a319dfa3da1286f3dca312603be
ギン <197370556@stc.vtc.edu.hk>
2020-07-10 02:05:18 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <div class="row">
+      <div class="col">123</div>
+      <div class="col">234</div>
+    </div>
+    <!--    <img src="./assets/logo.png">-->
+    <!--    <HelloWorld/>-->
+  </div>
+</template>
+
+<script>
+    import HelloWorld from './components/HelloWorld'
+    import Cookies from 'js-cookie'
+
+    export default {
+        name: 'App',
+        components: {
+            HelloWorld
+        },
+        data: {
+            login: false,
+            id: null
+        },
+        mounted() {
+            // Check Login
+            const id = cookies.get("id")
+            if (id) {
+                this.login = true
+                this.id = id
+            }
+        }
+    }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
